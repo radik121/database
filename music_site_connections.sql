ffbe9e6ac6353cf6a08ad3a27238993d0132e5b2
Radik <radik121@mail.ru>
2021-12-20 21:14:09 +0300

    add db  music site connections

---
+++ music_site_connections.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS Artists (
+	id SERIAL PRIMARY KEY,
+	name_artist VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Albums (
+	id SERIAL PRIMARY KEY,
+	name_album VARCHAR(255) NOT NULL,
+	year_album INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Tracks (
+	id SERIAL PRIMARY KEY,
+	name_track VARCHAR(255) NOT NULL,
+	time_track INT NOT NULL,
+	id_album INT REFERENCES Albums(id)
+);
+
+CREATE TABLE IF NOT EXISTS Genres (
+	id SERIAL PRIMARY KEY,
+	name_genre VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS Collection (
+	id SERIAL PRIMARY KEY,
+	name_collection VARCHAR(255) NOT NULL UNIQUE,
+	year_collection INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ArtistsAlbums (
+	id SERIAL PRIMARY KEY,
+	id_artist INT REFERENCES Artists(id),
+	id_album INT REFERENCES Albums(id)
+);
+
+CREATE TABLE IF NOT EXISTS ArtistsGenres (
+	id_artist INT REFERENCES Artists(id),
+	id_genre INT REFERENCES Genres(id),
+	CONSTRAINT pk PRIMARY KEY (id_artist, id_genre)
+);
+
+CREATE TABLE IF NOT EXISTS CollectionsTracks (
+	id_collection INT PRIMARY KEY,
+	id_track INT NOT NULL
+);
